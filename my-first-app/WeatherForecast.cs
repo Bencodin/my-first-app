e4575afcc13cf1c6d2bd84545ea6d7a955923e78
Bencodin <benji9863@gmail.com>
2025-02-04 12:34:48 +0100

    let s failed a build

--- my-first-app/WeatherForecast.cs
+++ my-first-app/WeatherForecast.cs
@@ -1,17 +1,4 @@
 using System;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace myfirstapp
 {
@@ -25,4 +12,3 @@ namespace myfirstapp
 
         public string Summary { get; set; }
     }
-}
