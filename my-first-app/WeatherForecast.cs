979cc8cc27a4ac773b55a3518b2f2f3125fe6ca9
Bencodin <benji9863@gmail.com>
2025-02-04 12:36:58 +0100

    lets reconfigure the build

--- my-first-app/WeatherForecast.cs
+++ my-first-app/WeatherForecast.cs
@@ -1,4 +1,17 @@
 using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace myfirstapp
 {
@@ -12,3 +25,4 @@ namespace myfirstapp
 
         public string Summary { get; set; }
     }
+}
